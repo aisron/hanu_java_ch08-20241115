41d907b4096fdea9b84f3fb43f0d19141fe0917a
aisron <aisron@naver.com>
2024-11-15 15:48:02 +0900

    System클래스 현재시간

---
+++ src/ch11_01/MainClass.java
@@ -0,0 +1,13 @@
+package ch11_01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 시간은 Long으로 선언
+		Long time1 = System.currentTimeMillis();
+		System.out.println(time1);
+	}
+
+}
