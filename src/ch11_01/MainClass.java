23195582293fd6207b816bfdca082cb6048f5a4b
aisron <aisron@naver.com>
2024-11-15 15:53:23 +0900

    현재 시간을 1/1000 초 단위의 값으로 반환

--- src/ch11_01/MainClass.java
+++ src/ch11_01/MainClass.java
@@ -6,8 +6,21 @@ public class MainClass {
 		// TODO Auto-generated method stub
 
 		// 시간은 Long으로 선언
-		Long time1 = System.currentTimeMillis();
+		Long time1 = System.currentTimeMillis();  // 현재 시간을 1/1000 초 단위의 값으로 반환
 		System.out.println(time1);
+		
+		int sum = 0;
+		for(int i = 1; i<=1000000;i++) {
+			sum= sum+i;
+			
+		}
+		System.out.println(sum);
+		
+		Long time2 = System.currentTimeMillis();
+		System.out.println(time2);
+		
+		System.out.println(time2-time1);
+		
 	}
 
 }
